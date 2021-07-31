d0e53801c56d5a535481b62aa64b5877fb2d9279
Mrigesh Dasgupta <dasguptamrigesh@gmail.com>
2021-07-31 18:38:54 -0400

    Island Package

---
+++ src/main/java/Island/Island.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Island;
+
+/**
+ *
+ * @author mrigesh
+ */
+public class Island {
+    
+    private int centerX;
+    private int centerY;
+
+    public Island(int centerX, int centerY) {
+        //Defined center of Island
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
+    public int getCenterX() {
+        return centerX;
+    }
+
+    public void setCenterX(int centerX) {
+        this.centerX = centerX;
+    }
+
+    public int getCenterY() {
+        return centerY;
+    }
+
+    public void setCenterY(int centerY) {
+        this.centerY = centerY;
+    }
+    
+}
