0a8ddc6b5410bd137e58e30585f6710017e17e3a
Mrigesh Dasgupta <dasguptamrigesh@gmail.com>
2021-08-08 01:39:35 -0400

    Created mutationColor List api

--- src/main/java/Mutation/Mutation.java
+++ src/main/java/Mutation/Mutation.java
@@ -7,27 +7,24 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class Mutation {
 
-
-    public static int infection_factor;   //U = host_value
-    //public static int genotypefitnessValue;
-    public static HashMap<String, Color> variantColor= new HashMap<String, Color>();;
+    public static HashMap<Integer, Color> mutationColor = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-       int s = calculateGenotypeFitness("1BCDEFGHIJ",93);
-        int q = calculateGenotypeFitness("1BCDEFGHIJ",97);
+        int s = calculateGenotypeFitness("1BCDEFGHIJ", 93);
+        int q = calculateGenotypeFitness("1BCDEFGHIJ", 97);
     }
-
 
     public static int calculateGenotypeFitness(String genotype, int infection_factor) throws IOException {
         Ini ini = new Ini(new File("./config.properties"));
         Map<String, String> gene_length = ini.get("gene_length");
         Map<String, String> base_value = ini.get("base_value");
         int baseVal = Integer.parseInt(base_value.get("base"));
-        int genotypefitnessValue=0;
-        int gene_fitness_value=0;
+        int genotypefitnessValue = 0;
+        int gene_fitness_value = 0;
         boolean islarger = genotype.length() > 10 ? true : false;
         int loop = genotype.length() > 10 ? genotype.length() : genotype.length();
 
@@ -62,6 +59,21 @@ public class Mutation {
         }
         return genotypefitnessValue;
     }
+
+
+
+    //Inserts every mutation into this HashMap and assigns random color
+    public static void insertIntoMutationList(int mutationCount) {
+        Random r = new Random();
+        mutationColor.put(mutationCount, new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+    }
+
+    //Fetch the color based on mutation count
+    public static Color fetchmutationColor(int mutationCount) {
+        return mutationColor.get(mutationCount);
+    }
+
+
 
 
 
